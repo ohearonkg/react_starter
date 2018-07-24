0a8dd8b265583ee855d568e9885aabf99c5a0a44
K.G. O'Hearon <ohearonkg@gmail.com>
2018-07-24 15:38:04 -0300

    now with prettier working!

--- src/components/HelloList/HelloList.tsx
+++ src/components/HelloList/HelloList.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import styled from 'react-emotion';
-import Hello, { HelloProps } from '../Hello/Hello';
+import * as React from "react";
+import styled from "react-emotion";
+import Hello, { HelloProps } from "../Hello/Hello";
 
 interface IHelloList {
   /**
    * A list of hello objects
    */
-  hellos:           HelloProps[];
+  hellos: HelloProps[];
 }
 
 const HelloListWrapper = styled.div`
@@ -16,11 +16,11 @@ const HelloListWrapper = styled.div`
 const HelloList = (props: IHelloList) => {
   return (
     <HelloListWrapper>
-    {props.hellos.map( (hello:HelloProps, index:number ) => 
-        <Hello name={hello.name} age={hello.age} key={index}/>
-      )}
+      {props.hellos.map((hello: HelloProps, index: number) => (
+        <Hello name={hello.name} age={hello.age} key={index} />
+      ))}
     </HelloListWrapper>
-  )
-}
+  );
+};
 
 export default HelloList;
